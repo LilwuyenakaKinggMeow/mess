2b795e58f44222ec5fb26d3a844e51acbcbd12d4
Tên Của Bạn <email@v d.com>
2024-12-03 13:54:48 +0700

    a

---
+++ flutter_application_1/api/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Kết nối MongoDB
+mongoose.connect('mongodb+srv://nguyenso1:messingu@cluster0.nzd6o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+}).then(() => {
+    console.log('Connected to MongoDB Atlas');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Thêm middleware để parse JSON requests
+
+// Định nghĩa route cho các API
+const tripRoute = require('./routes/trip.route');
+const addressRoute = require('./routes/address.route');
+const userRoute = require('./routes/users.route');
+
+app.use('/user', userRoute);
+app.use('/address', addressRoute);
+app.use('/trip', tripRoute);
+
+// Route chính
+app.get('/', (req, res) => res.send(' Node.js'));
+
+// Khởi động server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
